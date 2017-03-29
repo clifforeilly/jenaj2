1940a05eea0a441a6580f29c21e0c3114358fdd8
co17 <clifforeilly@gmail.com>
2017-03-29 17:05:53 +0100

    update 4

--- src/main/java/jenaj2.java
+++ src/main/java/jenaj2.java
@@ -2,8 +2,14 @@ import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
+import org.apache.jena.reasoner.rulesys.Rule;
 
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -11,42 +17,86 @@ import java.util.List;
  */
 public class jenaj2 {
 
+    static String now;
+
     public static void main (String[] args) {
 
         try {
+            now = getNow();
+
+            String outputFile = "C:\\Users\\co17\\LocalStuff\\MyStuff\\Projects\\jenaj2";
+
+            String ns_DocStruct = "http://repositori.com/sw/onto/DocStruct.owl";
             OntModel mod_DocStruct = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
-            mod_DocStruct.read("http://repositori.com/sw/onto/DocStruct.owl");
+            mod_DocStruct.read(ns_DocStruct);
 
+            String ns_gate = "http://repositori.com/sw/onto/gate.owl";
             OntModel mod_gate = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
-            mod_gate.read("http://repositori.com/sw/onto/gate.owl");
+            mod_gate.read(ns_gate);
 
-            List<OntClass> oc = mod_DocStruct.listClasses().toList();
-            for(int i = 0 ; i<oc.size() ; i++)
-            {
-                System.out.println(oc.get(i).getLocalName());
-            }
-
-            oc = mod_gate.listClasses().toList();
-            for(int i = 0 ; i<oc.size() ; i++)
-            {
-                System.out.println(oc.get(i).getLocalName());
-            }
+            String ns_LassoingRhetoric = "http://repositori.com/sw/onto/LassoingRhetoric.owl";
+            OntModel mod_LassoingRhetoric = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
+            mod_LassoingRhetoric.read(ns_LassoingRhetoric);
 
 
+            String ns = "http://repositori.com/sw/onto/jj_" + now + ".owl";
             OntModel mod_new = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
+            //Resource resource = mod_new.createResource(ns);
 
-            OntClass Doc  = mod_DocStruct.getOntClass("Doc");
-            OntClass Sentence  = mod_gate.getOntClass("Sentence");
+            OntClass Doc  = mod_DocStruct.getOntClass(ns_DocStruct + "#Doc");
+            OntClass Sentence  = mod_gate.getOntClass(ns_gate + "#Sentence");
+            OntClass word  = mod_gate.getOntClass(ns_gate + "#word");
 
-            Individual i1 = mod_new.createIndividual(Doc);
+            OntClass t1 = mod_new.createClass(ns + "#test1");
+            Individual i1 = mod_new.createIndividual(ns + "#i1", t1);
+            Individual i2 = mod_new.createIndividual(ns + "#d1", Doc);
+            Individual i3 = mod_new.createIndividual(ns + "#s1", Sentence);
+            //Individual i3 = mod_new.createIndividual(ns + "#s1", word);
 
-            System.out.println(mod_new.toString());
+            String outputformat = "RDF/XML-ABBREV"; //Turtle RDF/XML RDF/XML-ABBREV
 
+            String rules = "@prefix j0: <" + ns_DocStruct + "#> . @prefix j1: <" + ns_gate + "#> . [rule1: (?x rdf:type j.0:Doc) -> (?x rdf:type j.1:word)]";
+            printIt(rules);
+
+            Reasoner reasoner = new GenericRuleReasoner((Rule.parseRules(rules)));
+            reasoner.setDerivationLogging(true);
+
+            InfModel inf1 = ModelFactory.createInfModel(reasoner, mod_new);
+
+            StmtIterator iter = inf1.listStatements();
+            while(iter.hasNext()){
+                Statement stmt = iter.nextStatement();
+                printIt(stmt.getSubject().getLocalName() + " --- " + stmt.getObject().asResource().getLocalName());
+            }
+            //printIt("----- Doc Struct ----------------------------------------");
+            //mod_DocStruct.write(System.out, outputformat);
+            //printIt("");
+            //printIt("----- gate ----------------------------------------");
+            //mod_gate.write(System.out, outputformat);
+            //printIt("");
+            printIt("----- new ----------------------------------------");
+            mod_new.write(System.out, outputformat);
+
+            FileWriter out = new FileWriter( outputFile + "\\jj_" + now + ".owl" );
+            mod_new.write(out, outputformat);
+            out.close();
 
         } catch (Exception exc) {
-            System.out.println(exc.getStackTrace());
+            System.out.println(exc.getMessage());
         }
 
+    }
+
+    public static void printIt(String text)
+    {
+        System.out.println(text);
+    }
+
+    public static String getNow()
+    {
+        Date date = new Date();
+        String modifiedDate= new SimpleDateFormat("yyyyMMddHHmmsss").format(date);
+        return modifiedDate;
     }
 
 }
